39259683610b0ec25fe1b4c153f83fdb29d65410
smileperez <aa.efimenko@yandex.ru>
2023-01-15 22:51:29 +0300

    complete second task

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,26 @@
-import orderByProps from './orderByProps';
+import destructorCharacter from './destructorCharacter';
 
-const obj = {
-  health: 10,
-  level: 2,
-  attack: 80,
-  name: 'мечник',
-  defence: 40,
+const character = {
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 3,
+  attack: 40,
+  defence: 10,
+  special: [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      // <- обратите внимание, описание "засекречено"
+    },
+  ],
 };
-const pattern = ['name', 'level'];
 
-console.log(orderByProps(obj, pattern));
+console.log(destructorCharacter(character));
