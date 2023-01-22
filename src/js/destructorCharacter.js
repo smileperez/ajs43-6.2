9552b0aa461e29219f621a9c856c3ab262fd30bc
smileperez <aa.efimenko@yandex.ru>
2023-01-22 17:14:55 +0300

    fix task

--- src/js/destructorCharacter.js
+++ src/js/destructorCharacter.js
@@ -1,17 +1,12 @@
 export default function destructorCharacter(obj) {
   const result = [];
-  const { special: [item1, item2] } = obj;
 
-  function destructorItems(arr) {
-    const {
-      id, name, icon, description = 'Описание недоступно',
-    } = arr;
-    return {
-      id, name, icon, description,
-    };
+  const { special } = obj;
+
+  for (const prop in special) {
+    const {id, name, icon, description = 'Описание недоступно' } = special[prop];
+    result.push( {id, name, icon, description} );
   }
-
-  result.push(destructorItems(item1), destructorItems(item2));
 
   return result;
 }
